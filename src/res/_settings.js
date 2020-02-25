e4049c71bbc1acc8f585cd876a1e40ff7cf83163
Raku Zeta <zetaraku@gmail.com>
2020-02-25 21:23:21 +0800

    webspace temporary workaround

--- src/res/_settings.js
+++ src/res/_settings.js
@@ -1,3 +1,3 @@
 // EDIT ME
-export const remote_data_host = 'https://ztrk1600.000webhostapp.com/data/';
+export const remote_data_host = 'https://raw.githubusercontent.com/zetaraku/ncucf-tmp-data/master';
 export const ga_tracking_id = 'UA-83813049-1';
