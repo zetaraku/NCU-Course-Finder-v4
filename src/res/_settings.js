10145cfca114e398639d114c8be3fc10f8f8b6a6
Raku Zeta <zetaraku@gmail.com>
2020-09-02 17:33:55 +0800

    change data source

--- src/res/_settings.js
+++ src/res/_settings.js
@@ -1,3 +1,3 @@
 // EDIT ME
-export const remote_data_host = 'https://raw.githubusercontent.com/zetaraku/ncucf-tmp-data/master';
+export const remote_data_host = 'https://ncucf-data.s3.amazonaws.com/data';
 export const ga_tracking_id = 'UA-83813049-1';
