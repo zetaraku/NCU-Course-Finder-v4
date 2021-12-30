19c36b8e198194d68ebdc75df6b2635aed3ba99c
Raku Zeta <zetaraku@gmail.com>
2021-12-30 10:56:21 +0800

    add new version announcement

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -46,7 +46,7 @@ export default class App extends React.Component {
 	render() {
 		return (
 			<div className="App">
-				{/* <a href="https://ncucf-v5.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+				<a href="https://ncucf.zetaraku.dev/" target="_blank" rel="noopener noreferrer">
 					<div style={{
 						display: 'flex',
 						justifyContent: 'center',
@@ -54,9 +54,9 @@ export default class App extends React.Component {
 						backgroundColor: 'gold',
 						height: '50px',
 					}}>
-						試用最新版本！－ NCU Course Finder 5.0 (beta)
+						前往使用最新版本！－ NCU Course Finder v6
 					</div>
-				</a> */}
+				</a>
 				{/* {
 					this.state.popupInfo !== null &&
 					<InfoModal
